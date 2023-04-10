f4a73b129ec83ecaf97a1aa5afc46aa8280403fe
nghiemtrang <125233841+nghiemtrang@users.noreply.github.com>
2023-04-10 16:49:17 +0700

    Card hover add

--- src/components/Card/CardItem.jsx
+++ src/components/Card/CardItem.jsx
@@ -11,7 +11,7 @@ export default function ({src, alt, text1, text2, text3}) {
     <div className="absolute left-0 bottom-0 p-6 xl:p-9 text-[#1d1d1d] lg:max-w-[60%]">
       <p className="text-[13px] xl:text-[15px] leading-5 mb-[10px] xl:mb-3 ">{text1}</p>
       <h3>
-        <a className="block text-[30px] xl:text-[38px] leading-9 font-medium underline xl:no-underline pb-[3px] mb-4 " href="">{text2}</a>
+        <a className="block text-[30px] xl:text-[38px] leading-9 font-medium underline xl:no-underline xl:hover:underline pb-[3px] mb-4 " href="">{text2}</a>
       </h3>
       <p className="text-[14px] xl:text-[17px]  leading-6 ">{text3}</p>
     </div>
