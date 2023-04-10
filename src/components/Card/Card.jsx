911492d4421f1fc276b21ef7f80395b7d283b786
nghiemtrang <125233841+nghiemtrang@users.noreply.github.com>
2023-04-10 16:45:41 +0700

    Card update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,40 +1,18 @@
+import CardItem from "./CardItem";
+
 export default function Card() {
   return (
     <section>
       {/* Container */}
-      <div className="px-[15px] mt-12">
+      <div className="max-w-screen-2xl mx-auto px-[15px] lg:px-[30px] xl:px-10 md:mt-[60px] xl:mt-20 ">
         {/* Card container */}
-        <div className="grid grid-rows-2 gap-3">
+        <div className="grid grid-col-1 lg:grid-cols-2 gap-3">
           {/* Card-1 */}
-          <div className="">
-            <img 
-              className="relative rounded-[10px] w-full"
-              src="https://cdn.shopify.com/s/files/1/0572/4146/8989/files/image-with-text_2_78a9c08d-7860-4d3e-bc54-02166e29f5ef.jpg?v=1656425838&width=840"
-              alt="Card-1"
-            />
-            <div className="absolute left-0 bottom-0 p-6">
-              <p>Whole grain</p>
-              <h3>
-                <a href="">Bakery</a>
-              </h3>
-              <p>Limited Availability</p>
-            </div>
-          </div>
+          <CardItem src="https://cdn.shopify.com/s/files/1/0572/4146/8989/files/image-with-text_2_78a9c08d-7860-4d3e-bc54-02166e29f5ef.jpg?v=1656425838&width=840" alt="Card-1" text1="Whole grain" text2="Bakery" text3="Limited Availability"/>
 
           {/* Card-2 */}
-          <div>
-            <img 
-              src="https://cdn.shopify.com/s/files/1/0572/4146/8989/files/image-with-text-_1_b4855ca5-7846-4df8-a74d-b854765b0e48.jpg?v=1655132129&width=840"
-              alt="Card-2"
-            />
-            <div>
-              <p>Hand-picked</p>
-              <h3>
-                <a href="">Fruits & Vegetables</a>
-              </h3>
-              <p>Limited Availability</p>
-            </div>
-          </div>
+          <CardItem src="https://cdn.shopify.com/s/files/1/0572/4146/8989/files/image-with-text-_1_b4855ca5-7846-4df8-a74d-b854765b0e48.jpg?v=1655132129&width=840" alt="Card-1" text1="Hand-picked" text2="Fruits & Vegetables" text3="Limited Availability"/>
+
         </div>
       </div>
     </section>
---
+++ src/components/Card/CardItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ({src, alt, text1, text2, text3}) {
+  return (
+    <div className="relative h-[346px] " >
+    <img 
+      className=" z-[-1] rounded-[10px] w-full h-full object-cover"
+      src={src}
+      alt={alt}
+    />
+    <div className="absolute left-0 bottom-0 p-6 xl:p-9 text-[#1d1d1d] lg:max-w-[60%]">
+      <p className="text-[13px] xl:text-[15px] leading-5 mb-[10px] xl:mb-3 ">{text1}</p>
+      <h3>
+        <a className="block text-[30px] xl:text-[38px] leading-9 font-medium underline xl:no-underline pb-[3px] mb-4 " href="">{text2}</a>
+      </h3>
+      <p className="text-[14px] xl:text-[17px]  leading-6 ">{text3}</p>
+    </div>
+  </div>
+  )
+}
