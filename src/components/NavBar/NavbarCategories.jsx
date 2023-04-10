69216a31b874b69819134054db857b995fe3d0ab
nghiemtrang <125233841+nghiemtrang@users.noreply.github.com>
2023-04-10 15:13:52 +0700

    Navbar update

---
+++ src/components/NavBar/NavbarCategories.jsx
@@ -0,0 +1,99 @@
+export default function NavbarCategories() {
+  return (
+    <div>
+      {/* Categories link list */}
+      <div>
+        {/* Pantry */}
+        <ul>
+          <li>
+            <span className="font-medium">
+              {" "}
+              <a href="">Pantry</a>
+            </span>
+          </li>
+          <li>
+            <a href="">All</a>
+          </li>
+          <li>
+            <a href="">Pasta & Noodles</a>
+          </li>
+          <li>
+            <a href="">Grains & Beans</a>
+          </li>
+          <li>
+            <a href="">Snacks</a>
+          </li>
+          <li>
+            <a href="">Oil, Vinegar & Spices</a>
+          </li>
+          <li>
+            <a href="">Sauces</a>
+          </li>
+          <li>
+            <a href="">Dressings</a>
+          </li>
+        </ul>
+
+        {/* Produce */}
+        <ul>
+          <li>
+            <span className="font-medium">
+              {" "}
+              <a href="">Produce</a>
+            </span>
+          </li>
+          <li>
+            <a href="">All</a>
+          </li>
+          <li>
+            <a href="">Pasta & Noodles</a>
+          </li>
+          <li>
+            <a href="">Grains & Beans</a>
+          </li>
+          <li>
+            <a href="">Snacks</a>
+          </li>
+
+          <li>
+            <span className="font-medium">
+              {" "}
+              <a href="">Produce</a>
+            </span>
+          </li>
+          <li>
+            <a href="">All</a>
+          </li>
+          <li>
+            <a href="">Pasta & Noodles</a>
+          </li>
+          <li>
+            <a href="">Grains & Beans</a>
+          </li>
+          <li>
+            <a href="">Snacks</a>
+          </li>
+          <li>
+            <a href="">Snacks</a>
+          </li>
+          <li>
+            <a href="">Snacks</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Categories image */}
+      <div>
+        <div>
+          <img
+            src="	https://cdn.shopify.com/s/files/1/0572/4146/8989/files/promo2.jpg?v=1654694174&width=860"
+            alt=""
+          />
+          <p>Fresh & Energetic</p>
+          <p>Healthy & Organic</p>
+          <button>Learn more</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NavBar/NavbarLink.jsx
@@ -0,0 +1,40 @@
+export default function NavbarLink() {
+    return (
+        <ul className="sm:hidden md:hidden lg:hidden xl:flex xl:justify-between xl:text-base xl:font-medium ">
+            <li className="xl:py-[15px] xl:px-5 xl:flex items-center">
+              <a className="font-inherit " href="">Categories</a>
+              <svg
+                className="inline-block sm:ml-[10px]"
+                width="13"
+                height="8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.414.086 7.9 6.57 6.485 7.985 0 1.5 1.414.086Z"
+                  fill="#000"
+                ></path>
+                <path
+                  d="M12.985 1.515 6.5 8 5.085 6.586 11.571.101l1.414 1.414Z"
+                  fill="#000"
+                ></path>
+              </svg>
+            </li>
+            <li className="xl:py-[15px] xl:px-5">
+              <a className="xl:pb-[3px]" href="">About us</a>
+            </li>
+            <li className="xl:py-[15px] xl:px-5">
+              <a className="xl:pb-[3px]" href="">Our stores</a>
+            </li>
+            <li className="xl:py-[15px] xl:px-5">
+              <a className="xl:pb-[3px]" href="">Our stores</a>
+            </li>
+            <li className="xl:py-[15px] xl:px-5">
+              <a className="xl:pb-[3px]" href="">Our stores</a>
+            </li>
+            <li className="xl:py-[15px] xl:px-5">
+              <a className="xl:pb-[3px]" href="">Our stores</a>
+            </li>
+          </ul>
+    )
+}
