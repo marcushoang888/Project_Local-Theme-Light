8e5112e55c31fcfa1612def5052100eae10536ce
nghiemtrang <125233841+nghiemtrang@users.noreply.github.com>
2023-04-11 16:52:47 +0700

    Nav update

--- src/components/NavBar/NavbarCategories.jsx
+++ src/components/NavBar/NavbarCategories.jsx
@@ -1,97 +1,79 @@
+import Categorie from "./Categorie";
+
 export default function NavbarCategories() {
   return (
-    <div>
-      {/* Categories link list */}
-      <div>
+    <div className="mx-auto pt-[45px] pb-[30px] ">
+      <div className="grid grid-cols-4">
+        {/* Categories link list */}
         {/* Pantry */}
-        <ul>
-          <li>
-            <span className="font-medium">
-              {" "}
-              <a href="">Pantry</a>
-            </span>
-          </li>
-          <li>
-            <a href="">All</a>
-          </li>
-          <li>
-            <a href="">Pasta & Noodles</a>
-          </li>
-          <li>
-            <a href="">Grains & Beans</a>
-          </li>
-          <li>
-            <a href="">Snacks</a>
-          </li>
-          <li>
-            <a href="">Oil, Vinegar & Spices</a>
-          </li>
-          <li>
-            <a href="">Sauces</a>
-          </li>
-          <li>
-            <a href="">Dressings</a>
-          </li>
-        </ul>
-
-        {/* Produce */}
-        <ul>
-          <li>
-            <span className="font-medium">
-              {" "}
-              <a href="">Produce</a>
-            </span>
-          </li>
-          <li>
-            <a href="">All</a>
-          </li>
-          <li>
-            <a href="">Pasta & Noodles</a>
-          </li>
-          <li>
-            <a href="">Grains & Beans</a>
-          </li>
-          <li>
-            <a href="">Snacks</a>
-          </li>
-
-          <li>
-            <span className="font-medium">
-              {" "}
-              <a href="">Produce</a>
-            </span>
-          </li>
-          <li>
-            <a href="">All</a>
-          </li>
-          <li>
-            <a href="">Pasta & Noodles</a>
-          </li>
-          <li>
-            <a href="">Grains & Beans</a>
-          </li>
-          <li>
-            <a href="">Snacks</a>
-          </li>
-          <li>
-            <a href="">Snacks</a>
-          </li>
-          <li>
-            <a href="">Snacks</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* Categories image */}
-      <div>
         <div>
-          <img
-            src="	https://cdn.shopify.com/s/files/1/0572/4146/8989/files/promo2.jpg?v=1654694174&width=860"
-            alt=""
+          <Categorie
+            heading="Pantry"
+            lilink={[
+              "All",
+              "Pasta & Noodles",
+              "Grains & Beans",
+              "Snacks",
+              "Oil, Vinegar & Spices",
+              "Sauces",
+              "Dressings",
+            ]}
           />
-          <p>Fresh & Energetic</p>
-          <p>Healthy & Organic</p>
-          <button>Learn more</button>
+        </div>
+
+        {/* Produce & Dairy&Eggs */}
+        <div>
+          <Categorie
+            heading="Produce"
+            lilink={["All", "Vegetable", "Meat", "Herb & Aromatics"]}
+          />
+
+          <Categorie
+            heading="Dairy & Eggs"
+            lilink={[
+              "All",
+              "Milk & Cream",
+              "Eggs & Butter",
+              "Cheese",
+              "Yogurt & Cultured Dairy",
+              "Plant-Based",
+            ]}
+          />
+        </div>
+
+        {/* Bakery & Drin */}
+        <div>
+          <Categorie
+            heading="Bakery"
+            lilink={[
+              "All",
+              "Break",
+              "Bun & Rolls",
+              "Bagels & Breakfast",
+              "Gluten-Free",
+            ]}
+          />
+
+          <Categorie
+            heading="Dairy & Eggs"
+            lilink={["All", "Coffee", "Tea & Elixirs", "Juices"]}
+          />
+        </div>
+
+        {/* Categories image */}
+        <div>
+          <div className="relative ml-[15px]">
+            <img
+              className="rounded-[10px] w-100%   object-center"
+              src="https://cdn.shopify.com/s/files/1/0572/4146/8989/files/promo2.jpg?v=1654694174&width=860"
+              alt=""
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-center text-[#1d1d1d] font-medium">
+              <p className=" mb-3 text-[25px] leading-7 ">Fresh & Energetic</p>
+              <p className=" mb-3 text-[15px] leading-[22px]">Healthy & Organic</p>
+              <button>Learn more</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
